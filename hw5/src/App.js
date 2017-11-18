1186d24a283c33774f4b985dd9e6d1febc2f09b5
Andrey Shemyakov <andrey.shemiakov@gmail.com>
2017-11-19 01:13:36 +0300

    HW5

--- hw5/src/App.js
+++ hw5/src/App.js
@@ -1,27 +1,59 @@
 import React, {Component} from 'react';
 import './App.css';
 import {addListener, removeListener, isAuthorized} from './AuthorizeApi';
+import {Link, Redirect, Route, Switch} from 'react-router-dom';
+import Home from './Home';
+import Auth from "./Auth";
+import Private from './Private';
+import Public from './Public';
 
 class App extends Component {
-  state = {
-    isAuthorized
-  };
+    state = {
+        isAuthorized
+    };
 
-  componentDidMount() {
-    addListener(this.handleAuthorize);
-  }
+    componentDidMount() {
+        addListener(this.handleAuthorize);
+    }
 
-  componentWillUnmount() {
-    removeListener(this.handleAuthorize);
-  }
+    componentWillUnmount() {
+        removeListener(this.handleAuthorize);
+    }
 
-  handleAuthorize = isAuthorized => {
-    this.setState({isAuthorized});
-  };
+    handleAuthorize = isAuthorized => {
+        this.setState({isAuthorized});
+    };
 
-  render() {
-    return null;
-  }
+    render() {
+        const {isAuthorized} = this.state;
+
+        return (
+            <div>
+                <ul>
+                    <li><Link to="/auth">Войти</Link></li>
+                    <li><Link to="/private">Секретная страница</Link></li>
+                    <li><Link to="/public">Публичная страница</Link></li>
+                    <li><Link to="/">Главная</Link></li>
+                </ul>
+
+                <hr/>
+
+                <Switch>
+                    <Route exact path="/auth" component={Auth}/>
+                    <Route exact path="/public" component={Public}/>
+                    <Route exact path="/" component={Home}/>
+                    {isAuthorized ? (
+                        <Route exact path="/private" component={Private}/>
+                    ) : (
+                        <Redirect from="/private" to="/auth"/>
+                    )}
+                    {!isAuthorized ? (
+                        <Redirect from="*" to="/"/>
+                    ) : undefined}
+                </Switch>
+            </div>
+        );
+    }
 }
 
 export default App;
--- hw5/src/Auth.js
+++ hw5/src/Auth.js
@@ -1,9 +1,47 @@
 import React, {Component} from 'react';
+import {authorizeUser} from './AuthorizeApi';
+import {Redirect} from "react-router-dom";
 
 class Auth extends Component {
-  render() {
-    return null;
-  }
+    state = {
+        email: '',
+        password: '',
+        isAuthorized: false,
+        isError: false
+    };
+
+    handleSubmit = () => {
+        const {email, password} = this.state;
+        const isAuthorized = authorizeUser(email, password);
+        this.setState(state => ({
+            isAuthorized: isAuthorized,
+            isError: !isAuthorized
+        }));
+    };
+
+    handleChangeForm = (event) => {
+        const {name, value} = event.target;
+        this.setState({[name]: value});
+    };
+
+    render() {
+        const {email, password, isAuthorized, isError} = this.state;
+
+        return (
+            <div>
+                <div>
+                    <div>
+                        <input name="email" placeholder="email" value={email} onChange={this.handleChangeForm}/>
+                        <input name="password" placeholder="password" value={password}
+                               onChange={this.handleChangeForm}/>
+                        {isError ? (<p className="error">Неверный пароль и/или почта.</p>) : undefined}
+                    </div>
+                    <button onClick={this.handleSubmit}>Submit</button>
+                </div>
+                {isAuthorized ? <Redirect to="/"/> : undefined}
+            </div>
+        );
+    }
 }
 
 export default Auth;
